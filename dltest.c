1eea89b05cb5ad0f55a0f3ad3fbbd0d781692e6c
Sijmen J. Mulder <ik@sjmulder.nl>
2020-08-22 20:53:07 +0200

    Rewrite, more or less
     - Accept list of libraries
     - 'Quiet' and 'all' flags
     - Portability

--- dltest.c
+++ dltest.c
@@ -1,27 +1,61 @@
 #include <stdio.h>
+#include <getopt.h>
 #include <dlfcn.h>
+
+#ifdef RTLD_NOLOAD
+# define DLFLAGS	(RTLD_LAZY | RTLD_NOLOAD)
+#else
+# define DLFLAGS	(RTLD_LAZY)
+#endif
+
+#ifdef __APPLE__
+# include <AvailabilityMacros.h>
+# if MAC_OS_X_VERSION_MIN_REQUIRED >= 1050
+#  define HAVE_PREFLIGHT
+# endif
+#endif
 
 int
 main(int argc, char **argv)
 {
-	int flags;
+	int opt_quiet = 0, opt_all = 0;
+	int ch, ok, num_ok = 0;
+#ifndef HAVE_PREFLIGHT
 	void *dl;
+#endif
 
-	if (argc != 2) {
-		fputs("usage: dltest <path>\n", stderr);
+	while ((ch = getopt(argc, argv, "aq")) != -1) {
+		switch (ch) {
+			case 'a': opt_all = 1; break;
+			case 'q': opt_quiet = 1; break;
+			default: return 64;
+		}
+	}
+
+	argc -= optind;
+	argv += optind;
+
+	if (!argc) {
+		fputs("usage: dltest library [...]", stderr);
 		return 64;
 	}
 
-	flags = RTLD_LAZY;
-#ifdef RTLD_NOLOAD
-	flags |= RTLD_NOLOAD;
+	for (; *argv; argv++) {
+#ifdef HAVE_PREFLIGHT
+		ok = dlopen_preflight(*argv);
+#else
+		if (!(ok = dl = dlopen(*argv, DLFLAGS)))
+			dlclose(dl);
 #endif
+		if (ok)
+			num_ok++;
 
-	if (!(dl = dlopen(argv[1], RTLD_LAZY))) {
-		fprintf(stderr, "%s\n", dlerror());
-		return 1;
+		if (ok && !opt_quiet)
+			puts(*argv);
+		else if (!ok && !opt_quiet)
+			fprintf(stderr, "%s\n", dlerror());
+
 	}
 
-	dlclose(dl);
-	return 0;
+	return !(opt_all ? (num_ok == argc) : num_ok);
 }
